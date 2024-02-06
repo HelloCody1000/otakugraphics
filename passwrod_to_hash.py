6aa180a3bdc25264e3d0ca1de3b51c94f4ff0f30
HelloCody1000 <70619796+HelloCody1000@users.noreply.github.com>
2024-02-06 00:15:17 -0600

    Create passwrod_to_hash.py

---
+++ passwrod_to_hash.py
@@ -0,0 +1,9 @@
+import bcrypt
+
+def hash_password(plain_text_password):
+    # Generate a salt
+    salt = bcrypt.gensalt()
+    
+    # Hash the password
+    hashed_password = bcrypt.hashpw(plain_text_password.encode('utf-8'), salt)
+    return hashed_password
