21f8ad84fe6ed50d98b0af7207ea7f6a14f6e500
HelloCody1000 <codyfry@me.com>
2024-02-08 16:53:15 -0600

    test

--- passwordHash.py
+++ passwordHash.py
@@ -5,5 +5,6 @@ def hash_password(plain_text_password):
     salt = bcrypt.gensalt()
 
     # Hash the password
+    #comment test
     hashed_password = bcrypt.hashpw(plain_text_password.encode('utf-8'), salt)
     return hashed_password
